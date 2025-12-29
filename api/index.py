0cf23cd580a08cb5eafc151174db03732722227f
Vedant <vedantmahajan2017@gmail.com>
2025-12-29 19:51:52 +0530

    updation for deployment

--- api/index.py
+++ api/index.py
@@ -1,13 +1,1 @@
-from flask import Flask, request, jsonify
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "ML Music Genre App is running!"
-
-@app.route("/predict", methods=["POST"])
-def predict():
-    data = request.json
-    # load model here
-    return jsonify({"genre": "rock"})
+from app.app import app
